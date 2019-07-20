c67b8d994d1fc31b5bf9e55571b3c003cf4c4fa2
darylGranger <darylgranger@gmail.com>
2019-07-20 15:11:02 -0700

    army-list component added

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
 Vue.config.devtools = true;
 
+Vue.component('army-list', {
+    props: {
+        army: {
+            type: Object,
+            required: true
+        }
+    },
+    template: `
+        <div>
+            <h2>Your Army</h2>
+            <h3>Caster</h3>
+                <ul>
+                    <li>{{ army.caster }}</li>
+                </ul>
+            <h3>Beasts</h3>
+            <ul>
+                <li v-for="beast in army.beasts">{{ beast }}</li>
+            </ul>
+            <h3>Units</h3>
+            <ul>
+                <li v-for="unit in army.units">{{ unit }}</li>
+            </ul>
+            <h3>Solo</h3>
+            <ul>
+                <li>{{ army.solo }}</li>
+            </ul>
+            
+        </div>
+    `
+})
+
+
+
 var app = new Vue ({
     el: "#app",
     data: {
+        army: {
         caster: null,
-        beasts: [],
-        units: [],
+        beasts: [null, null, null],
+        units: [null, null, null],
         solo: null,
+        },
         casterOptions: ['Calandra', 'Madrak', 'Jarl'],
         beastOptions: ['Mauler', 'Bomber', 'Night Troll'],
         unitOptions: ['Champions', 'Warders', 'Pyg Burrowers'],
@@ -14,31 +49,32 @@ var app = new Vue ({
     },
     methods: {
         pickCaster: function(caster) {
-            this.caster = caster;
+            this.army.caster = caster;
         },
         pickBeast: function(beast) {
-           this.beasts.push(beast);
-           
-           if (this.beasts.length > 3) {
-               this.beasts.splice(0, 1);
+           this.army.beasts.push(beast);
+
+           if (this.army.beasts.length > 3) {
+               this.army.beasts.splice(0, 1);
            }
         },
         pickUnit: function(unit) {
-            this.units.push(unit);
+            this.army.units.push(unit);
 
-            if (this.units.length > 3) {
-                this.units.splice(0, 1);
+            if (this.army.units.length > 3) {
+                this.army.units.splice(0, 1);
             }
         },
         pickSolo: function(solo) {
-            this.solo = solo;
+            this.army.solo = solo;
         },
         resetArmy: function() {
-            this.caster = null;
-            this.beasts = [];
-            this.units = [];
-            this.solo = null;
+            this.army.caster = null;
+            this.army.beasts = [null,null,null];
+            this.army.units = [null,null,null];
+            this.army.solo = null;
         }
     }
 })
+
 
